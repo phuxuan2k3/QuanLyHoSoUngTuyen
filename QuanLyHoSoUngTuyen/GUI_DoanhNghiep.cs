285791d33675bfe8171720ee9ac36a3e4c4450bc
21120190 <quocankg2611@gmail.com>
2024-07-08 21:25:52 +0700

    feat

--- QuanLyHoSoUngTuyen/GUI_DoanhNghiep.cs
+++ QuanLyHoSoUngTuyen/GUI_DoanhNghiep.cs
@@ -1,5 +1,6 @@
 ﻿using Ctrler.DoanhNghiep;
 using Ctrler.NhanVienTiepNhan;
+using DTO;
 using GUI.Styles;
 using GUI.UserControls;
 
@@ -7,57 +8,66 @@ namespace GUI;
 
 public partial class GUI_DoanhNghiep : Form
 {
-    // todo: Thay bang tai khoan duoc dang nhap
-    private string _maDN = "1";
-    public string MaDN { get => _maDN; set => _maDN = value; }
+	// todo: Thay bang tai khoan duoc dang nhap
+	private string _maDN = "1";
+	public string MaDN { get => _maDN; set => _maDN = value; }
+	public TrangThaiDoanhNghiep TrangThaiDoanhNghiep { get; set; }
 
 	public GUI_DoanhNghiep()
 	{
 		Style.ApplyStylingEventOnChildControlAdded(this, ButtonStyle.Apply);
 		_instance = this;
 		InitializeComponent();
+		if (TrangThaiDoanhNghiep == TrangThaiDoanhNghiep.ChuaXacThuc || TrangThaiDoanhNghiep == TrangThaiDoanhNghiep.KhongHopLe)
+		{
+			btnCapNhatTTDN.Enabled = true;
+			btnDanhSachThongTinDangTuyen.Enabled = false;
+			btnDuyetHoSo.Enabled = false;
+			btnTaoYeuCau.Enabled = false;
+		}
+		else
+		{
+			btnCapNhatTTDN.Enabled = true;
+			btnDanhSachThongTinDangTuyen.Enabled = true;
+			btnDuyetHoSo.Enabled = true;
+			btnTaoYeuCau.Enabled = true;
+		}
 	}
 
-    private void btnTaoYeuCau_Click(object sender, EventArgs e)
-    {
-        var gui = new GUI_DienThongTinDangTuyen();
-        gui.HienThi(new Ctrler_DienThongTinDangTuyen());
-        SwitchContent(gui);
-    }
+	private void btnTaoYeuCau_Click(object sender, EventArgs e)
+	{
+		var gui = new GUI_DienThongTinDangTuyen();
+		gui.HienThi(new Ctrler_DienThongTinDangTuyen());
+		SwitchContent(gui);
+	}
 
-    private void btnDanhSachThongTinDangTuyen_Click(object sender, EventArgs e)
-    {
-        var gui = new GUI_DanhSachThongTinDangTuyen();
-        gui.HienThi(new Ctrler_DanhSachThongTinDangTuyen(_maDN));
-        SwitchContent(gui);
-    }
+	private void btnDanhSachThongTinDangTuyen_Click(object sender, EventArgs e)
+	{
+		var gui = new GUI_DanhSachThongTinDangTuyen();
+		gui.HienThi(new Ctrler_DanhSachThongTinDangTuyen(_maDN));
+		SwitchContent(gui);
+	}
 
-    public void SwitchContent(UserControl content)
-    {
-        _contentPanel.Controls.Clear();
-        _contentPanel.Controls.Add(content);
-        content.Dock = DockStyle.Fill;
-    }
+	public void SwitchContent(UserControl content)
+	{
+		_contentPanel.Controls.Clear();
+		_contentPanel.Controls.Add(content);
+		content.Dock = DockStyle.Fill;
+	}
 
-    private static GUI_DoanhNghiep? _instance = null;
+	private void btnXacThucDN_Click(object sender, EventArgs e)
+	{
+		GUI_XacThucThongTinDoanhNghiep content = new();
+		content.HienThi();
+		SwitchContent(content);
+	}
 
-
-
-    private void btnXacThucDN_Click(object sender, EventArgs e)
-    {
-        GUI_XacThucThongTinDoanhNghiep content = new();
-        content.HienThi();
-        SwitchContent(content);
-    }
-
-    private void btnXetDuyetTTDT_Click(object sender, EventArgs e)
-    {
-        GUI_DanhSachTTDTCanXetDuyet content = new();
-        content.HienThi(new Ctrler_DanhSachTTDTCanXetDuyet());
-        SwitchContent(content);
-    }
-
-
+	private void btnXetDuyetTTDT_Click(object sender, EventArgs e)
+	{
+		GUI_DanhSachTTDTCanXetDuyet content = new();
+		content.HienThi(new Ctrler_DanhSachTTDTCanXetDuyet());
+		SwitchContent(content);
+	}
 
 	private void btnCapNhatTTDN_Click(object sender, EventArgs e)
 	{
@@ -66,12 +76,13 @@ public partial class GUI_DoanhNghiep : Form
 		SwitchContent(content);
 	}
 
-    private void btnDuyetHoSo_Click(object sender, EventArgs e)
-    {
-        GUI_DanhSachThongTinDangTuyen_DN content = new();
-        content.HienThi(new Ctrler_DanhSachThongTinDangTuyen(""));
-        SwitchContent(content);
-    }
+	private void btnDuyetHoSo_Click(object sender, EventArgs e)
+	{
+		GUI_DanhSachThongTinDangTuyen_DN content = new();
+		content.HienThi(new Ctrler_DanhSachThongTinDangTuyen(""));
+		SwitchContent(content);
+	}
 
-    public static GUI_DoanhNghiep Instance => _instance ??= new GUI_DoanhNghiep();
+	private static GUI_DoanhNghiep? _instance = null;
+	public static GUI_DoanhNghiep Instance => _instance ??= new GUI_DoanhNghiep();
 }
--- QuanLyHoSoUngTuyen/GUI_XacThucNguoiDung.cs
+++ QuanLyHoSoUngTuyen/GUI_XacThucNguoiDung.cs
@@ -15,112 +15,113 @@ using Utilis;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
 namespace GUI
 {
-    public partial class GUI_XacThucNguoiDung : Form
-    {
-        public GUI_XacThucNguoiDung()
-        {
-            InitializeComponent();
-            KhoiTaoManHinh();
-        }
+	public partial class GUI_XacThucNguoiDung : Form
+	{
+		public GUI_XacThucNguoiDung()
+		{
+			InitializeComponent();
+			KhoiTaoManHinh();
+		}
 
-        private void KhoiTaoManHinh()
-        {
-            GUI_DangNhap gui_DangNhap = new(DangNhapThanhCong!, DangKy!);
-            SwitchContent(gui_DangNhap);
-        }
+		private void KhoiTaoManHinh()
+		{
+			GUI_DangNhap gui_DangNhap = new(DangNhapThanhCong!, DangKy!);
+			SwitchContent(gui_DangNhap);
+		}
 
-        private void GUI_XacThucNguoiDung_Load(object sender, EventArgs e)
-        {
-        }
+		private void GUI_XacThucNguoiDung_Load(object sender, EventArgs e)
+		{
+		}
 
-        //protected override void OnResize(EventArgs e)
-        //{
-        //    base.OnResize(e);
+		//protected override void OnResize(EventArgs e)
+		//{
+		//    base.OnResize(e);
 
-        //    foreach (Control cnt in Controls)
-        //    {
-        //        ResizeAll(cnt, Size);
-        //    }
+		//    foreach (Control cnt in Controls)
+		//    {
+		//        ResizeAll(cnt, Size);
+		//    }
 
-        //    oldSize = Size;
-        //}
+		//    oldSize = Size;
+		//}
 
-        //private void ResizeAll(Control control, Size newSize)
-        //{
-        //    if (oldSize.Width == 0 || oldSize.Height == 0)
-        //    {
-        //        return;
-        //    }
-        //    int widthChange = newSize.Width - oldSize.Width;
-        //    control.Left += (control.Left * widthChange) / oldSize.Width;
-        //    control.Width += (control.Width * widthChange) / oldSize.Width;
+		//private void ResizeAll(Control control, Size newSize)
+		//{
+		//    if (oldSize.Width == 0 || oldSize.Height == 0)
+		//    {
+		//        return;
+		//    }
+		//    int widthChange = newSize.Width - oldSize.Width;
+		//    control.Left += (control.Left * widthChange) / oldSize.Width;
+		//    control.Width += (control.Width * widthChange) / oldSize.Width;
 
-        //    int heightChange = newSize.Height - oldSize.Height;
-        //    control.Top += (control.Top * heightChange) / oldSize.Height;
-        //    control.Height += (control.Height * heightChange) / oldSize.Height;
-        //}
+		//    int heightChange = newSize.Height - oldSize.Height;
+		//    control.Top += (control.Top * heightChange) / oldSize.Height;
+		//    control.Height += (control.Height * heightChange) / oldSize.Height;
+		//}
 
-        private void DangNhapThanhCong(object sender, EventArgs e)
-        {
-            switch ((e as EventArgs<DTO_TaiKhoan>)!.Value.VaiTro)
-            {
-                case "Doanh nghiệp":
-                    var dn = BUS_DoanhNghiep.LayDoanhNghiep((e as EventArgs<DTO_TaiKhoan>)!.Value.TenTaiKhoan);
-                    GUI_DoanhNghiep gUI_DoanhNghiep = new GUI_DoanhNghiep();
-                    gUI_DoanhNghiep.MaDN = dn.MaDN;
-                    Hide();
-                    gUI_DoanhNghiep.ShowDialog();
-                    Show();
-                    break;
-                case "Ban lãnh đạo":
-                    GUI_BanLanhDao gui_BanLanhDao = new GUI_BanLanhDao();
-                    Hide();
-                    gui_BanLanhDao.ShowDialog();
-                    Show();
-                    break;
-                case "Nhân viên nghiệp vụ":
-                    GUI_NhanVienNghiepVu gui_NhanVienNghiepVu = new GUI_NhanVienNghiepVu();
-                    Hide();
-                    gui_NhanVienNghiepVu.ShowDialog();
-                    Show();
-                    break;
-                case "Nhân viên tiếp nhận":
-                    GUI_NhanVienTiepNhan gui_NhanVienTiepNhan = new GUI_NhanVienTiepNhan();
-                    Hide();
-                    gui_NhanVienTiepNhan.ShowDialog();
-                    Show();
-                    break;
-                case "Nhân viên thanh toán":
-                    GUI_NhanVienThanhToan gui_NhanVienThanhToan = new GUI_NhanVienThanhToan();
-                    Hide();
-                    gui_NhanVienThanhToan.ShowDialog();
-                    Show();
-                    break;
-                case "Ứng viên":
-                    GUI_UngVien gui_UngVien = new GUI_UngVien();
-                    Hide();
-                    gui_UngVien.ShowDialog();
-                    Show();
-                    break;
-            }
-        }
-        private void DangKy(object sender, EventArgs e)
-        {
-            GUI_DangKy gUI_DangKy = new GUI_DangKy(BackToDangNhap!);
-            SwitchContent(gUI_DangKy);
-        }
+		private void DangNhapThanhCong(object sender, EventArgs e)
+		{
+			switch ((e as EventArgs<DTO_TaiKhoan>)!.Value.VaiTro)
+			{
+				case "Doanh nghiệp":
+					var dn = BUS_DoanhNghiep.LayDoanhNghiep((e as EventArgs<DTO_TaiKhoan>)!.Value.TenTaiKhoan);
+					GUI_DoanhNghiep gUI_DoanhNghiep = new GUI_DoanhNghiep();
+					gUI_DoanhNghiep.MaDN = dn.MaDN;
+					gUI_DoanhNghiep.TrangThaiDoanhNghiep = dn.TrangThai;
+					Hide();
+					gUI_DoanhNghiep.ShowDialog();
+					Show();
+					break;
+				case "Ban lãnh đạo":
+					GUI_BanLanhDao gui_BanLanhDao = new GUI_BanLanhDao();
+					Hide();
+					gui_BanLanhDao.ShowDialog();
+					Show();
+					break;
+				case "Nhân viên nghiệp vụ":
+					GUI_NhanVienNghiepVu gui_NhanVienNghiepVu = new GUI_NhanVienNghiepVu();
+					Hide();
+					gui_NhanVienNghiepVu.ShowDialog();
+					Show();
+					break;
+				case "Nhân viên tiếp nhận":
+					GUI_NhanVienTiepNhan gui_NhanVienTiepNhan = new GUI_NhanVienTiepNhan();
+					Hide();
+					gui_NhanVienTiepNhan.ShowDialog();
+					Show();
+					break;
+				case "Nhân viên thanh toán":
+					GUI_NhanVienThanhToan gui_NhanVienThanhToan = new GUI_NhanVienThanhToan();
+					Hide();
+					gui_NhanVienThanhToan.ShowDialog();
+					Show();
+					break;
+				case "Ứng viên":
+					GUI_UngVien gui_UngVien = new GUI_UngVien();
+					Hide();
+					gui_UngVien.ShowDialog();
+					Show();
+					break;
+			}
+		}
+		private void DangKy(object sender, EventArgs e)
+		{
+			GUI_DangKy gUI_DangKy = new GUI_DangKy(BackToDangNhap!);
+			SwitchContent(gUI_DangKy);
+		}
 
-        private void BackToDangNhap(object sender, EventArgs e)
-        {
-            GUI_DangNhap gui_DangNhap = new(DangNhapThanhCong!, DangKy!);
-            SwitchContent(gui_DangNhap);
-        }
+		private void BackToDangNhap(object sender, EventArgs e)
+		{
+			GUI_DangNhap gui_DangNhap = new(DangNhapThanhCong!, DangKy!);
+			SwitchContent(gui_DangNhap);
+		}
 
-        private void SwitchContent(UserControl content)
-        {
-            pnContent.Controls.Clear();
-            pnContent.Controls.Add(content);
-            content.Dock = DockStyle.Fill;
-        }
-    }
+		private void SwitchContent(UserControl content)
+		{
+			pnContent.Controls.Clear();
+			pnContent.Controls.Add(content);
+			content.Dock = DockStyle.Fill;
+		}
+	}
 }
