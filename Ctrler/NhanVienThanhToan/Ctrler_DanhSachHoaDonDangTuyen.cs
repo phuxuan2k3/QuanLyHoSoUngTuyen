0d1a4d2eb1410bf4a5f611f417421ccb8892ea78
21120190 <quocankg2611@gmail.com>
2024-07-11 10:36:35 +0700

    fix thanh toan

--- Ctrler/NhanVienThanhToan/Ctrler_DanhSachHoaDonDangTuyen.cs
+++ Ctrler/NhanVienThanhToan/Ctrler_DanhSachHoaDonDangTuyen.cs
@@ -1,5 +1,7 @@
 ﻿using BUS;
+using DAO.Exceptions;
 using DTO;
+using System.Data.SqlClient;
 
 namespace Ctrler.NhanVienThanhToan
 {
@@ -12,7 +14,23 @@ namespace Ctrler.NhanVienThanhToan
 
 		public DTO_HoaDon? TruyVanHoaDon(string maTTDT)
 		{
-			return BUS_HoaDon.Lay(maTTDT);
+			if (maTTDT.All(char.IsDigit) == false)
+			{
+				throw new QueryException($"Mã TTDT truy vấn không được chứa ký hiệu không là số. Mã đã nhập: {maTTDT}.");
+			}
+			_ = int.TryParse(maTTDT, out int intMaTTDT);
+			if (!(intMaTTDT > 0 && intMaTTDT < int.MaxValue))
+			{
+				throw new QueryException($"Mã TTDT truy vấn phải là số nguyên và nằm trong khoảng 0 - {int.MaxValue}. Mã đã nhập: {maTTDT}.");
+			}
+			try
+			{
+				return BUS_HoaDon.Lay(maTTDT);
+			}
+			catch (SqlException)
+			{
+				throw new QueryException($"Mã TTDT truy vấn không phù hợp. Mã đã nhập: {maTTDT}.");
+			}
 		}
 
 		public void LoadChiTietHoaDon(string maTTDT, ref DTO_ThongTinDangTuyen thongTinDangTuyen, ref List<DTO_ChiTietHoaDon> chiTietHoaDons, ref DTO_DoanhNghiep doanhNghiep)
